10ac7ed9fde778584874a05b29f504963d91dde4
edowon0623@gmail.com <Dwlee$09>
2025-03-31 13:39:04 +0900

    removed unnecessary files

--- delivery-service/src/main/java/com/example/deliveryservice/jpa/DeliveryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.deliveryservice.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryRepository extends JpaRepository<DeliveryEntity, Long> {
-}
