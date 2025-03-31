4ce16759b318c6030b3889ac2b289ce868db87a4
edowon0623@gmail.com <Dwlee$09>
2025-03-31 11:59:14 +0900

    added the delivery service

---
+++ delivery-service/src/main/java/com/example/deliveryservice/jpa/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package com.example.deliveryservice.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<DeliveryEntity, Long> {
+}
