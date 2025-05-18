7c3c4aef49e58cbafeb0ee01e648407984064747
edowon0623@gmail.com <Dwlee$09>
2025-05-19 07:42:01 +0900

    removed an unnecessary code

--- user-service/src/test/java/com/example/userservice/service/UserServiceImplTest.java
+++ user-service/src/test/java/com/example/userservice/service/UserServiceImplTest.java
@@ -1,13 +1,9 @@
 package com.example.userservice.service;
 
-import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
-import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.RequestOrder;
-import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseOrder;
-import com.example.userservice.vo.ResponseUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,29 +27,12 @@ class UserServiceImplTest {
     private UserRepository userRepository;
 
     @Mock
-    private OrderServiceClient orderServiceClient;
-
-    @Mock
     BCryptPasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserServiceImpl userService;
 
     private UserDto savedUser;
-
-//    @BeforeEach
-//    void createUser() {
-//        userDto = new UserDto();
-//        userDto.setEmail("edowon0623@gmail.com");
-//        userDto.setName("Kenneth Lee");
-//        userDto.setPwd("12345678");
-//
-//        UserEntity userEntity = new UserEntity();
-//
-//        when(userService.createUser(userDto)).thenReturn(userDto);
-//
-//        when(userRepository.findByUserId(userDto.getUserId())).thenReturn(Optional.of(userEntity));
-//    }
 
     @BeforeEach
     void test_createUser() {
@@ -69,20 +48,5 @@ class UserServiceImplTest {
 
     @Test
     void test_getUserByUserId() {
-        // given
-        RequestOrder requestOrder = RequestOrder.builder()
-                .productId("test-product-id-001").qty(10).unitPrice(1200).build();
-        orderServiceClient.createOrder(savedUser.getUserId(), requestOrder);
-
-        List<ResponseOrder> ordersList = new ArrayList<>();
-        when(orderServiceClient.getOrders(any(String.class))).thenReturn(ordersList);
-
-        // when
-        UserDto userDto = userService.getUserByUserId(savedUser.getUserId());
-
-        // then
-        assertNotNull(userDto);
-        assertEquals(0, userDto.getOrders().size());
-        verify(orderServiceClient, times(1)).getOrders(any(String.class));
     }
 }
